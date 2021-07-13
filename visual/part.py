315be5fb58d503f4efb777cba19e679c25610b9b
wciq <1208640961@qq.com>
2021-07-13 11:53:24 +0800

    up：semi_supervised_adversarial_autoencoder.py 绘图

--- visual/part.py
+++ visual/part.py
@@ -3,7 +3,8 @@ import numpy as np
 # import mnist_inference
 import os
 import tqdm
-
+import matplotlib.pyplot as plt
+import cv2
 # 加载用于生成PROJECTOR日志的帮助函数
 from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.examples.tutorials.mnist import input_data
@@ -22,20 +23,76 @@ TENSOR_NAME = "FINAL_LOGITS"
 num = 500
 
 
+def imshow(frame, name="window", delay=0):
+    cv2.imshow(name, frame)
+    cv2.waitKey(delay=delay)
+
+
+# 使用给出的MNIST图片列表生成sprite图像
+def create_sprite_image(images):
+    """Returns a sprite image consisting of images passed as argument. Images should be count x width x height"""
+    if isinstance(images, list):
+        images = np.array(images)
+    img_h = images.shape[1]
+    img_w = images.shape[2]
+    # sprite图像可以理解成是小图片平成的大正方形矩阵，大正方形矩阵中的每一个元素就是原来的小图片。于是这个正方形的边长就是sqrt(n),其中n为小图片的数量。
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+
+    # 使用全1来初始化最终的大图片。
+    spriteimage = np.ones((img_h*n_plots, img_w*n_plots))
+
+    for i in range(n_plots):
+        for j in range(n_plots):
+            # 计算当前图片的编号
+            this_filter = i*n_plots + j
+            if this_filter < images.shape[0]:
+                # 将当前小图片的内容复制到最终的sprite图像
+                this_img = images[this_filter]
+                spriteimage[i*img_h:(i + 1)*img_h,
+                j*img_w:(j + 1)*img_w] = this_img
+
+    return spriteimage
+
+
+def build_meta_sprite():
+    # 加载MNIST数据。这里指定了one_hot=False,于是得到的labels就是一个数字，表示当前图片所表示的数字。
+    mnist = input_data.read_data_sets("./Data", one_hot=False)
+
+    # 生成sprite图像
+    to_visualise = 1 - np.reshape(mnist.test.images[:num], (-1, 28, 28))
+    sprite_image = create_sprite_image(to_visualise)
+
+    # 将生成的sprite图片放到相应的日志目录下
+    path_for_mnist_sprites = os.path.join(LOG_DIR, SPRITE_FILE)
+    plt.imsave(path_for_mnist_sprites, sprite_image, cmap='gray')
+    plt.imshow(sprite_image, cmap='gray')
+
+    # 生成每张图片对应的标签文件并写道相应的日志目录下
+    path_for_mnist_metadata = os.path.join(LOG_DIR, META_FIEL)
+    with open(path_for_mnist_metadata, 'w') as f:
+        # f.write("Index\tLabel\n")
+        # for index, label in enumerate(mnist.test.labels[:100]):
+        #     f.write("%d\t%d\n"%(index, label))
+
+        for index, label in enumerate(mnist.test.labels[:num]):
+            f.write("%d\n" % (label,))
+
+
 def get_result():
-    z_dim = 2
-    model_path = "/opt/py-project/Adversarial_Autoencoder/Results/Adversarial_Autoencoder/2021-07-12 19:09:56.496976_2_0.001_200_12_0.9_Adversarial_Autoencoder/Saved_models"
+    mnist = input_data.read_data_sets("./Data", one_hot=False)
+    z_dim = 3
+    model_path = "/opt/py-project/Adversarial_Autoencoder/Results/Adversarial_Autoencoder/2021-07-13 09:13:38.932779_3_0.001_500_100_0.9_Adversarial_Autoencoder/Saved_models"
     input_checkpoint = tf.train.latest_checkpoint(model_path)
     saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=True)
     with tf.Session() as sess:
         saver.restore(sess, save_path=input_checkpoint)
         op = sess.graph.get_tensor_by_name('Decoder_1/Sigmoid:0')
         decoder_input = sess.graph.get_tensor_by_name('Decoder_input:0')
-        results = np.zeros((1, 784))
-        for i in range(num):
-            result = sess.run(op, feed_dict={decoder_input: np.random.random((1, z_dim))})
-            results = np.vstack((results, result))
-        return results[1:]
+        x_input = sess.graph.get_tensor_by_name('Input:0')
+        encoder_output = sess.graph.get_tensor_by_name('Encoder/e_latent_variable/matmul_1:0')
+        decoder_output = sess.graph.get_tensor_by_name('Decoder/Sigmoid:0')
+        result = sess.run(encoder_output, feed_dict={x_input: mnist.test.images[:num]})
+        return result
 
 
 # 生成可视化最终输出层向量所需要的日志文件
@@ -85,8 +142,7 @@ def visualisation(final_result):
 # 主函数先调用模型训练的过程，再使用训练好的模型来处理MNIST测试数据，
 # 最后将得到的输出层矩阵输出到PROJECTOR需要的日志文件中。
 def main(argv=None):
-
-    # final_result = np.random.random((100, 784))
+    build_meta_sprite()
     final_result = get_result()
     visualisation(final_result)
 
